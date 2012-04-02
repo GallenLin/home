164a4cc81ae87c5586c0b77c598cb8cd6de8d947
gallen.lin <gallen.lin@netronixinc.com>
2012-04-02 23:50:09 +0800

    update install_home.sh .
    * rename .bashrc-gallen -> .bashrc_custom .
    * rename .bashrc -> .bashrc-ubuntu10.04-default .
    
    --HG--
    rename : .bashrc => .bashrc-ubuntu10.04-default
    rename : .bashrc-gallen => .bashrc_custom

--- install_home.sh
+++ install_home.sh
@@ -1,47 +1,71 @@
 #!/bin/bash
 
+
+
 work_dir="$(pwd)"
 #"
-find -maxdepth 1|grep -v "^\.svn"|grep -v "^\.hg"|grep -v "^\.git"|grep -v "\.$"|grep -v "\.\.$"|grep -v "$(basename ${0})"| 
-while read _path
-do
-	if [ -f "${HOME}/${_path}" ];then
-		LANG=C dialog --ascii-lines --title "${_path} in ${HOME} is existed" \
-		--clear \
-		--ok-label "Overwrite" \
-		--no-label "Abort" \
-		--extra-label "Keep" --extra-button \
-		--yesno "File/Folder named \"$(basename ${_path})\" in \"${HOME}\" is existed . \
-                 What you want to do ? " 15 61
-		case $? in
-  	0)
-    	echo "Overwrite chosen."
-			rm -f "${HOME}/${_path}"
-			ln -s "${work_dir}/${_path}" "${HOME}/"
-			;;
-  	1)
-    	echo "Abort chosen."
-			exit 1
-			;;
-  	2)
-    	echo "Help pressed.";;
-  	3)
-    	echo "Keep pressed.";;
-			
-  	255)
-    	echo "ESC pressed."
-			exit 1
-			;;
-		esac
 
-	elif [ -d "${HOME}/${_path}" ];then
-		echo "skip folder \"${HOME}/${_path}\""
-	else
-		ln -s "${work_dir}/${_path}" "${HOME}/"
-	fi
+install_bashrc_custom() {
+	addstr=". ~/.bashrc_custom"
 	
-done
+	if [ -z "$(cat ~/.bashrc|grep "${addstr}")" ];then
+		echo "" >> ~/.bashrc
+		echo "${addstr}" >> ~/.bashrc
+		echo "" >> ~/.bashrc
+	fi
 
-exit 0
+	return 0
+}
+
+install_files_to_home() {
+	find -maxdepth 1|grep -v "\.svn"|grep -v "\.hg"|grep -v "\.git"|grep -v "\.$"|grep -v "\.\.$"|grep -v "$(basename ${0})"| 
+	while read _path
+	do
+		if [ -f "${HOME}/${_path}" ];then
+			LANG=C dialog --ascii-lines --title "${_path} in ${HOME} is existed" \
+			--clear \
+			--ok-label "Overwrite" \
+			--no-label "Abort" \
+			--extra-label "Keep" --extra-button \
+			--yesno "File/Folder named \"$(basename ${_path})\" in \"${HOME}\" is existed . \
+									 What you want to do ? " 15 61
+			case $? in
+			0)
+				echo "Overwrite chosen."
+				rm -f "${HOME}/${_path}"
+				ln -s "${work_dir}/${_path}" "${HOME}/"
+				;;
+			1)
+				echo "Abort chosen."
+				return 1
+				;;
+			2)
+				echo "Help pressed.";;
+			3)
+				echo "Keep pressed.";;
+				
+			255)
+				echo "ESC pressed."
+				return 1
+				;;
+			esac
+
+		elif [ -d "${HOME}/${_path}" ];then
+			echo "skip folder \"${HOME}/${_path}\""
+		else
+			echo "${work_dir}/${_path} -> ${HOME}/"
+			ln -s "${work_dir}/${_path}" "${HOME}/"
+		fi
+		
+	done
+
+	return 0
+}
+
+install_files_to_home
+[ $? != 0 ] && echo "install files to home fail !" && exit 1
+
+install_bashrc_custom
+exit $?
 
 
