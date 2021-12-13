cc1bd051fca9d2a4bda89243c837ffd3ea826488
Gallen <gallen.lin@netronixinc.com>
2021-12-13 21:55:23 +0800

    test_switch_detect.py add timestamp .

--- raspbian/test_switch_detect.py
+++ raspbian/test_switch_detect.py
@@ -3,6 +3,7 @@
 from gpiozero import Button
 import time
 from signal import pause
+from datetime import datetime
 
 stat_chg_cnt = 0
 
@@ -12,9 +13,10 @@ timeout_secs = 1
 def sw_handle() :
 	global stat_chg_cnt
 	global sw
+	global sw_stat_before_chg
 
 	if stat_chg_cnt==0:
-	    print("sw val=",sw.value)
+	    sw_stat_before_chg = sw.value
 	cur_time=time.clock_gettime(time.CLOCK_BOOTTIME)
 	stat_chg_cnt+=1
 	last_time = cur_time
@@ -30,14 +32,16 @@ def sw_off_handle() :
 sw=Button(21,pull_up=False)
 sw.when_pressed = sw_on_handle
 sw.when_released = sw_off_handle
+sw_stat_before_chg = sw.value;
 
 while 1 : 
 	time.sleep(1)
 	if stat_chg_cnt > 0 :
 		cur_time=time.clock_gettime(time.CLOCK_BOOTTIME)
 		diff_time = cur_time - last_time
-		if diff_time > timeout_secs : 
-			print("sw chg=",stat_chg_cnt,",sw val=",sw.value)
+		if diff_time > timeout_secs :
+			now = datetime.now()
+			print(now,"switch changed cnt=",stat_chg_cnt,",",sw_stat_before_chg,"->",sw.value)
 			stat_chg_cnt = 0
 	
 #time.sleep(1)
