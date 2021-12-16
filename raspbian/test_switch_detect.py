e8286a12d62b43d02f00a3d712d748a818f77acb
Gallen <gallen.lin@netronixinc.com>
2021-12-16 20:29:39 +0800

    add times records and timestamp .

--- raspbian/test_switch_detect.py
+++ raspbian/test_switch_detect.py
@@ -6,20 +6,31 @@ from signal import pause
 from datetime import datetime
 
 stat_chg_cnt = 0
-
+max_time_records = 20
 last_time=time.clock_gettime(time.CLOCK_BOOTTIME)
+test_begin_time = datetime.now()
 timeout_secs = 1
+spend_times = []
 
 def sw_handle() :
 	global stat_chg_cnt
 	global sw
 	global sw_stat_before_chg
+	global last_time
+	global max_time_records
+	global test_begin_time
 
 	if stat_chg_cnt==0:
-	    sw_stat_before_chg = sw.value
+		sw_stat_before_chg = sw.value
+		test_begin_time = datetime.now()
+		
 	cur_time=time.clock_gettime(time.CLOCK_BOOTTIME)
 	stat_chg_cnt+=1
+	t = cur_time-last_time
+	if len(spend_times) <= max_time_records :
+		spend_times.append(round(t,6))
 	last_time = cur_time
+        
 
 def sw_on_handle() :
 	#print("sw on")
@@ -41,7 +52,8 @@ while 1 :
 		diff_time = cur_time - last_time
 		if diff_time > timeout_secs :
 			now = datetime.now()
-			print(now,"switch changed cnt=",stat_chg_cnt,",",sw_stat_before_chg,"->",sw.value)
+			print(test_begin_time,"->",now,"(duration=",now-test_begin_time,"):","switch changed cnt=",stat_chg_cnt,",",sw_stat_before_chg,"->",sw.value)
+			print("spend times = ",spend_times)
 			stat_chg_cnt = 0
 	
 #time.sleep(1)
